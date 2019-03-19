479e12d40a49b3056f1c3b2ad957cee03512c426
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2019-03-19 14:06:05 -0700

    use httpbin.default as the service name

--- examples/example-tls/istioClientScript.sh
+++ examples/example-tls/istioClientScript.sh
@@ -2,5 +2,5 @@
 
 for i in `seq 1 ${COUNT:=20}`
 do
-    /build/install/example-tls/bin/hello-world-tls-client proto1server 54440 /etc/certs/root-cert.pem /etc/certs/cert-chain.pem /etc/certs/key.pem
+    /build/install/example-tls/bin/hello-world-tls-client httpbin.default 54440 /etc/certs/root-cert.pem /etc/certs/cert-chain.pem /etc/certs/key.pem
 done
