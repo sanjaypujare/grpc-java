f47b0e07d8df44bfc04a7178d6ba57a49011f2f5
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2019-09-27 15:57:40 -0700

    Create the protocol negotiators under xds/tls along the lines of AltsProtocolNegotiator

---
+++ xds/src/main/java/io/grpc/xds/tls/SdsProtocolNegotiators.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2019 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.xds.tls;
+
+import io.grpc.netty.GrpcHttp2ConnectionHandler;
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.InternalNettyChannelBuilder;
+import io.grpc.netty.InternalProtocolNegotiator;
+import io.grpc.netty.InternalProtocolNegotiator.ProtocolNegotiator;
+import io.grpc.netty.InternalProtocolNegotiators;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.ssl.SslContext;
+import io.netty.util.AsciiString;
+import java.io.InputStream;
+import java.util.logging.Logger;
+import javax.net.ssl.SSLException;
+
+/**
+ * A gRPC {@link ProtocolNegotiator} used to handle SSL when certs/keys are
+ * provided by SDS. This class creates a Netty handler similar to
+ * Netty's {@code SslHandler}.
+ */
+public final class SdsProtocolNegotiators {
+  private static final Logger logger = Logger.getLogger(SdsProtocolNegotiators.class.getName());
+
+  private static final AsciiString SCHEME = AsciiString.of("https");
+
+  public interface Cfg {
+
+    InputStream getKeyCertChainInputStream();
+
+    InputStream getKeyInputStream();
+
+    InputStream getTrustChainInputStream();
+  }
+
+  /**
+   * ClientSdsProtocolNegotiatorFactory is a factory for doing client side negotiation.
+   */
+  public static final class ClientSdsProtocolNegotiatorFactory
+      implements InternalNettyChannelBuilder.ProtocolNegotiatorFactory {
+    // inject SecretManager once ready
+
+    Cfg cfg;
+
+    public ClientSdsProtocolNegotiatorFactory(Cfg cfg) {
+      this.cfg = cfg;
+    }
+
+    @Override
+    public InternalProtocolNegotiator.ProtocolNegotiator buildProtocolNegotiator() {
+      final ClientSdsProtocolNegotiator negotiator = new ClientSdsProtocolNegotiator(cfg);
+      final class LocalSdsNegotiator implements InternalProtocolNegotiator.ProtocolNegotiator {
+
+        @Override
+        public AsciiString scheme() {
+          return negotiator.scheme();
+        }
+
+        @Override
+        public ChannelHandler newHandler(GrpcHttp2ConnectionHandler grpcHandler) {
+          return negotiator.newHandler(grpcHandler);
+        }
+
+        @Override
+        public void close() {
+          negotiator.close();
+        }
+      }
+
+      return new LocalSdsNegotiator();
+    }
+
+  }
+
+  private static final class ClientSdsProtocolNegotiator implements ProtocolNegotiator {
+    final Cfg cfg;
+
+    // private final ObjectPool<SdsClient> sdsClientPool;
+    // private SdsClient sdsClient;
+    public ClientSdsProtocolNegotiator(
+        /*ObjectPool<SdsClient> sdsClientPool*/ Cfg cfg) {
+      // this.sdsClientPool = sdsClientPool;
+      // this.sdsClient = sdsClientPool.getObject();
+      this.cfg = cfg;
+      System.out.println("from ClientSdsProtocolNegotiator ctor");
+    }
+
+    @Override
+    public void close() {
+      // sdsClient = sdsClientPool.returnObject(sdsClient);
+      System.out.println("from ClientSdsProtocolNegotiator close");
+    }
+
+    @Override
+    public ChannelHandler newHandler(GrpcHttp2ConnectionHandler grpcHandler) {
+      return new ClientSdsHandler(/*sdsClient,*/ grpcHandler, cfg);
+    }
+
+    @Override
+    public AsciiString scheme() {
+      return SCHEME;
+    }
+  }
+
+  private static final class ClientSdsHandler extends ChannelHandlerAdapter {
+    // private final SdsClient sdsClient;
+    private final GrpcHttp2ConnectionHandler grpcHandler;
+    private final Cfg cfg;
+
+    ClientSdsHandler(
+        /*SdsClient sdsClient,*/ GrpcHttp2ConnectionHandler grpcHandler,
+        Cfg cfg) {
+      // this.sdsClient = sdsClient;
+      this.grpcHandler = grpcHandler;
+      this.cfg = cfg;
+    }
+
+    @Override
+    public void handlerAdded(final ChannelHandlerContext ctx) {
+      /*SdsSecretConfig sdsConfig = grpcHandler.getEagAttributes().get(SDS_CONFIG_KEY);
+      if (sdsConfig == null) {
+        ctx.fireExceptionCaught(Status.UNAVAILABLE.withDescription(
+        “No SDS info available; not using xDS balancer”).toException());
+        return;
+      } */
+      // Cfg could just be SslContext; made it a separate object to show we can pass
+      // whatever data is needed.
+
+      // instead of using cfg.getTrustChainInputStream() to trustManager we use
+      // SdsTrustManagerFactory
+      SslContext sslContext = null;
+      try {
+        sslContext =
+            GrpcSslContexts.forClient()
+                .trustManager(new SdsTrustManagerFactory(cfg.getTrustChainInputStream()))
+                .keyManager(cfg.getKeyCertChainInputStream(), cfg.getKeyInputStream())
+                .build();
+      } catch (SSLException e) {
+        throw new RuntimeException(e);
+      }
+      ChannelHandler handler = InternalProtocolNegotiators.tls(sslContext).newHandler(grpcHandler);
+      // Delegate rest of handshake to TLS handler
+      ctx.pipeline().replace(ClientSdsHandler.this, null, handler);
+    }
+  }
+}
---
+++ xds/src/main/java/io/grpc/xds/tls/SdsX509TrustManager.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2015 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.xds.tls;
+
+import java.io.InputStream;
+import java.net.Socket;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Collection;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.X509ExtendedTrustManager;
+import javax.net.ssl.X509TrustManager;
+
+public class SdsX509TrustManager extends X509ExtendedTrustManager implements X509TrustManager {
+  final InputStream certChain;
+  X509Certificate[] x509Certs;
+
+  /**
+   * Construct mgr.
+   */
+  SdsX509TrustManager(InputStream certChain) {
+    this.certChain = certChain;
+    try {
+      CertificateFactory cf = CertificateFactory.getInstance("X.509");
+      Collection<? extends Certificate> certs = cf.generateCertificates(certChain);
+      x509Certs = certs.toArray(new X509Certificate[0]);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  @Override
+  public void checkClientTrusted(X509Certificate[] x509Certificates, String s, Socket socket)
+      throws CertificateException {
+    System.out.println("checkClientTrusted 1 called");
+  }
+
+  @Override
+  public void checkClientTrusted(X509Certificate[] x509Certificates, String s, SSLEngine sslEngine)
+          throws CertificateException {
+    System.out.println("checkClientTrusted 2 called");
+  }
+
+  @Override
+  public void checkClientTrusted(X509Certificate[] x509Certificates, String s)
+          throws CertificateException {
+    System.out.println("checkClientTrusted 3 called");
+  }
+
+  @Override
+  public void checkServerTrusted(X509Certificate[] x509Certificates, String s, Socket socket)
+      throws CertificateException {
+    System.out.println("checkServerTrusted 1 called");
+  }
+
+  @Override
+  public void checkServerTrusted(X509Certificate[] x509Certificates, String s, SSLEngine sslEngine)
+      throws CertificateException {
+    System.out.println("checkServerTrusted 2 called: " + s);
+  }
+
+  @Override
+  public void checkServerTrusted(X509Certificate[] x509Certificates, String s)
+      throws CertificateException {
+    System.out.println("checkServerTrusted 3 called");
+  }
+
+  @Override
+  public X509Certificate[] getAcceptedIssuers() {
+    System.out.println("getAcceptedIssuers called");
+    return x509Certs;
+  }
+}
