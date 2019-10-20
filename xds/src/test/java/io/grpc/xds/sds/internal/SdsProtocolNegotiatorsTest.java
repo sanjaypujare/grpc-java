ca322c7766c88ac2be14b8dce18cbbefd7dfc4d6
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2019-10-19 17:09:48 -0700

    unit test added for SdsClientHandler: need to clean up1

--- xds/src/test/java/io/grpc/xds/sds/internal/SdsProtocolNegotiatorsTest.java
+++ xds/src/test/java/io/grpc/xds/sds/internal/SdsProtocolNegotiatorsTest.java
@@ -19,16 +19,22 @@ package io.grpc.xds.sds.internal;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import com.google.common.base.Strings;
+import io.envoyproxy.envoy.api.v2.auth.CertificateValidationContext;
+import io.envoyproxy.envoy.api.v2.auth.CommonTlsContext;
+import io.envoyproxy.envoy.api.v2.auth.TlsCertificate;
+import io.envoyproxy.envoy.api.v2.auth.UpstreamTlsContext;
+import io.envoyproxy.envoy.api.v2.core.DataSource;
 import io.grpc.Attributes;
 import io.grpc.InternalChannelz.Security;
-import io.grpc.netty.GrpcHttp2ConnectionHandler;
+import io.grpc.internal.testing.TestUtils;
+import io.grpc.netty.*;
 import io.grpc.xds.sds.internal.SdsProtocolNegotiators.ClientSdsProtocolNegotiator;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelOutboundHandlerAdapter;
-import io.netty.channel.ChannelPromise;
+import io.netty.channel.*;
+import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.codec.http2.DefaultHttp2Connection;
 import io.netty.handler.codec.http2.DefaultHttp2ConnectionDecoder;
 import io.netty.handler.codec.http2.DefaultHttp2ConnectionEncoder;
@@ -37,7 +43,11 @@ import io.netty.handler.codec.http2.DefaultHttp2FrameWriter;
 import io.netty.handler.codec.http2.Http2ConnectionDecoder;
 import io.netty.handler.codec.http2.Http2ConnectionEncoder;
 import io.netty.handler.codec.http2.Http2Settings;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.handler.ssl.SslHandshakeCompletionEvent;
 import io.netty.util.internal.ObjectUtil;
+
+import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -50,14 +60,80 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class SdsProtocolNegotiatorsTest {
 
+  @SuppressWarnings("unused")  private static final String SERVER_1_PEM_FILE = "server1.pem";
+  @SuppressWarnings("unused")  private static final String SERVER_1_KEY_FILE = "server1.key";
+  private static final String CLIENT_PEM_FILE = "client.pem";
+  private static final String CLIENT_KEY_FILE = "client.key";
+  private static final String CA_PEM_FILE = "ca.pem";
+
   private final GrpcHttp2ConnectionHandler grpcHandler =
       FakeGrpcHttp2ConnectionHandler.newHandler();
+
+  private EmbeddedChannel channel = new EmbeddedChannel();
+  private ChannelPipeline pipeline = channel.pipeline();
+  @SuppressWarnings("unused")  private ChannelHandlerContext channelHandlerCtx;
 
   @Test
   public void clientSdsProtocolNegotiatorNewHandler_nullTlsContext() {
     ClientSdsProtocolNegotiator pn = new ClientSdsProtocolNegotiator(/* upstreamTlsContext= */ null);
     ChannelHandler newHandler = pn.newHandler(grpcHandler);
-    assertThat(newHandler).isInstanceOf();
+    assertThat(newHandler).isNotNull();
+    assertThat(newHandler.toString()).contains("io.grpc.netty.ProtocolNegotiators$WaitUntilActiveHandler");
+  }
+
+  @Test
+  public void clientSdsProtocolNegotiatorNewHandler_nonNullTlsContext() {
+    UpstreamTlsContext upstreamTlsContext = buildUpstreamTlsContext(
+            getCommonTlsContext(null, null));
+    ClientSdsProtocolNegotiator pn = new ClientSdsProtocolNegotiator(upstreamTlsContext);
+    ChannelHandler newHandler = pn.newHandler(grpcHandler);
+    assertThat(newHandler).isNotNull();
+    assertThat(newHandler.toString()).contains("io.grpc.xds.sds.internal.SdsProtocolNegotiators$ClientSdsHandler");
+  }
+
+  // logic from tlsHandler_userEventTriggeredSslEvent_supportedProtocolH2
+  @Test
+  public void clientSdsProtocolNegotiatorNewHandler_firePNEvent() throws IOException, InterruptedException {
+    @SuppressWarnings("unused")  ChannelHandlerContext grpcHandlerCtx = pipeline.context(grpcHandler);
+    @SuppressWarnings("unused")  final CountDownLatch latch = new CountDownLatch(1);
+    UpstreamTlsContext upstreamTlsContext =
+            buildUpstreamTlsContextFromFilenames(CLIENT_KEY_FILE, CLIENT_PEM_FILE, CA_PEM_FILE);
+
+    SdsProtocolNegotiators.ClientSdsHandler clientSdsHandler =
+            new SdsProtocolNegotiators.ClientSdsHandler(grpcHandler, upstreamTlsContext);
+    // copied from ProtocolNegotiatorsTest.tlsHandler_userEventTriggeredSslEvent_supportedProtocolH2
+    //pipeline.fireChannelActive();
+
+
+    pipeline.addLast(clientSdsHandler);
+    channelHandlerCtx = pipeline.context(clientSdsHandler);
+    assertNotNull(channelHandlerCtx);  // non-null since we just added it
+
+    // kick off protocol negotiation. (from io.grpc.netty.WriteBufferingAndExceptionHandler.handlerAdded)
+    pipeline.fireUserEventTriggered(InternalProtocolNegotiationEvent.getDefault());
+    // channelHandlerCtx = pipeline.context(clientSdsHandler);  //1
+    channel.runPendingTasks(); // need this for tasks to execute on eventLoop
+    //grpcHandlerCtx = pipeline.context(grpcHandler);
+    channelHandlerCtx = pipeline.context(clientSdsHandler);  //2
+    assertThat(channelHandlerCtx).isNull();  //
+    Object sslEvent = SslHandshakeCompletionEvent.SUCCESS;
+
+    pipeline.fireUserEventTriggered(sslEvent);
+    channel.runPendingTasks(); // need this for tasks to execute on eventLoop
+    assertTrue(channel.isOpen());
+    //grpcHandlerCtx = pipeline.context(grpcHandler);
+    //assertNotNull(grpcHandlerCtx);
+  }
+
+
+  /**
+   * Helper method to build UpstreamTlsContext for above tests. Called from other classes as well.
+   */
+  // todo: after rebase use the util method
+  private static UpstreamTlsContext buildUpstreamTlsContext(CommonTlsContext commonTlsContext) {
+    UpstreamTlsContext upstreamTlsContext =
+            UpstreamTlsContext.newBuilder().setCommonTlsContext(commonTlsContext).build();
+    return upstreamTlsContext;
   }
 
   private static final class FakeGrpcHttp2ConnectionHandler extends GrpcHttp2ConnectionHandler {
@@ -73,11 +149,12 @@ public class SdsProtocolNegotiatorsTest {
           /*channelUnused=*/ null, decoder, encoder, settings, false);
     }
 
+    @SuppressWarnings("unused")
     private final boolean noop;
-    private Attributes attrs;
-    private Security securityInfo;
-    private final CountDownLatch negotiated = new CountDownLatch(1);
-    private ChannelHandlerContext ctx;
+    //private Attributes attrs;
+    //private Security securityInfo;
+    //private final CountDownLatch negotiated = new CountDownLatch(1);
+    //private ChannelHandlerContext ctx;
 
     FakeGrpcHttp2ConnectionHandler(ChannelPromise channelUnused,
         Http2ConnectionDecoder decoder,
@@ -88,6 +165,55 @@ public class SdsProtocolNegotiatorsTest {
       this.noop = noop;
     }
 
+    @Override
+    public String getAuthority() {
+      return "authority";
+    }
   }
 
+  // TODO: following tests copied from another file: refactor to put in one place
+
+  private static String getTempFileNameForResourcesFile(String resFile) throws IOException {
+    return Strings.isNullOrEmpty(resFile) ? null : TestUtils.loadCert(resFile).getAbsolutePath();
+  }
+
+  /** Helper method to build UpstreamTlsContext for above tests. Called from other classes as well. */
+  static UpstreamTlsContext buildUpstreamTlsContextFromFilenames(String privateKey, String certChain,
+                                                                 String trustCa) throws IOException {
+    return buildUpstreamTlsContext(buildCommonTlsContextFromFilenames(privateKey, certChain,
+            trustCa));
+  }
+
+  static CommonTlsContext buildCommonTlsContextFromFilenames(String privateKey, String certChain,
+                                                             String trustCa) throws IOException {
+    TlsCertificate tlsCert = null;
+    privateKey = getTempFileNameForResourcesFile(privateKey);
+    certChain = getTempFileNameForResourcesFile(certChain);
+    trustCa = getTempFileNameForResourcesFile(trustCa);
+    if (!Strings.isNullOrEmpty(privateKey)
+            && !Strings.isNullOrEmpty(certChain)) {
+      tlsCert = TlsCertificate.newBuilder()
+              .setCertificateChain(DataSource.newBuilder().setFilename(certChain))
+              .setPrivateKey(DataSource.newBuilder().setFilename(privateKey))
+              .build();
+    }
+    CertificateValidationContext certContext = null;
+    if (!Strings.isNullOrEmpty(trustCa)) {
+      certContext = CertificateValidationContext.newBuilder()
+              .setTrustedCa(DataSource.newBuilder().setFilename(trustCa))
+              .build();
+    }
+    return getCommonTlsContext(tlsCert, certContext);
+  }
+
+  private static CommonTlsContext getCommonTlsContext(TlsCertificate tlsCertificate, CertificateValidationContext certContext) {
+    CommonTlsContext.Builder builder = CommonTlsContext.newBuilder();
+    if (tlsCertificate != null) {
+      builder = builder.addTlsCertificates(tlsCertificate);
+    }
+    if (certContext != null) {
+      builder = builder.setValidationContext(certContext);
+    }
+    return builder.build();
+  }
 }
