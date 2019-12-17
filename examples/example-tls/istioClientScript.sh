f0c224d5a7337798943e9c0b8e2da0351f0ef34d
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2019-12-16 17:36:55 -0800

    modify command line for new test: no certs to pass

--- examples/example-tls/istioClientScript.sh
+++ examples/example-tls/istioClientScript.sh
@@ -3,5 +3,5 @@
 for i in `seq 1 ${COUNT:=2000}`
 do
     echo Running test number $i
-    /build/install/example-tls/bin/hello-world-tls-client httpbin.default 54440 /etc/certs/root-cert.pem /etc/certs/cert-chain.pem /etc/certs/key.pem
+    /build/install/example-tls/bin/hello-world-tls-client httpbin.default 54440
 done
