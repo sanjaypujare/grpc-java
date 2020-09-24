e64f19c2b0e6efa700405aeac6f1b5a55769d6c1
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2020-09-23 22:25:20 -0700

    build zatar xds image - client

---
+++ examples/example-tls/buildZatarXdsImage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+IMAGENAME=zatar-grpc-xds
+TAG=1.00
+PROJECTID=meshca-gke-test
+
+echo Building ${IMAGENAME}:${TAG}
+
+docker build --no-cache -t zatar/${IMAGENAME}:${TAG} -f Dockerfile-xds.common .
+
+docker tag zatar/${IMAGENAME}:${TAG} gcr.io/${PROJECTID}/${IMAGENAME}:${TAG}
+
+docker push gcr.io/${PROJECTID}/${IMAGENAME}:${TAG}
