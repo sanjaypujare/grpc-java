04e4a57b0c9947f437769565de4014b78bceffcc
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2020-12-02 20:14:30 -0800

    more progress on ug-example

---
+++ examples/example-xds/ug-example/example-cleanup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+# this script deletes all the TD artifacts created in example-setup.sh
+
+gcloud alpha network-services endpoint-config-selectors delete ecs_mtls_psms --location=global -q
+
+gcloud alpha network-security server-tls-policies delete server_mtls_policy --location=global -q
+
+gcloud compute forwarding-rules delete example-grpc-forwarding-rule --global -q
+
+gcloud compute target-grpc-proxies delete example-grpc-proxy -q
+
+gcloud compute url-maps remove-host-rule example-grpc-url-map --host example-grpc-server:8000
+
+gcloud compute url-maps remove-path-matcher example-grpc-url-map --path-matcher-name example-grpc-path-matcher --global -q
+
+gcloud compute url-maps delete example-grpc-url-map --global -q
+
+gcloud compute backend-services delete example-grpc-service --global -q
+
+#gcloud alpha network-security client-tls-policies delete client_mtls_policy --location=global -q
+
+gcloud compute firewall-rules delete fw-allow-health-checks  -q
+
+gcloud compute health-checks delete example-health-check --global -q
+
+# now delete our deployment of service and client
+kubectl delete -f ug-example/gke-deployment.yaml
---
+++ examples/example-xds/ug-example/example-setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash -x
+
+# this script creates server side stuff
+
+# first deploy our service
+kubectl apply -f ug-example/gke-deployment.yaml
+
+sleep 20s
+
+NEG_NAME=$(gcloud beta compute network-endpoint-groups list | grep example-grpc-server | awk '{print $1}')
+
+if [ x${NEG_NAME} = x ]; then
+    echo NEG_NAME is not set, some issue with the deployment ug-steps-files/create-gke-service.yaml. Exiting...
+    exit 1
+fi
+
+# Give our service account access to TD APIs
+gcloud iam service-accounts add-iam-policy-binding \
+  --role roles/iam.workloadIdentityUser \
+  --member "serviceAccount:meshca-gke-test.svc.id.goog[default/example-grpc-server]" \
+  635862331669-compute@developer.gserviceaccount.com
+
+gcloud projects add-iam-policy-binding meshca-gke-test \
+   --member "serviceAccount:meshca-gke-test.svc.id.goog[default/example-grpc-server]" \
+   --role roles/compute.networkViewer
+
+gcloud compute health-checks create tcp example-health-check --use-serving-port
+
+#create the firewall rule for the health check to work. Note this one needs to be done
+#frequently because of GCP keeps deleting the firewall for security reasons
+gcloud compute firewall-rules create fw-allow-health-checks --network default --action ALLOW \
+    --direction INGRESS \
+    --source-ranges 35.191.0.0/16,130.211.0.0/22 \
+    --rules tcp
+
+# HTTP2 works but not GRPC...
+gcloud compute backend-services create example-grpc-service --global \
+    --health-checks example-health-check   --load-balancing-scheme INTERNAL_SELF_MANAGED --protocol HTTP2
+
+gcloud compute backend-services add-backend example-grpc-service --global \
+       --network-endpoint-group ${NEG_NAME} --network-endpoint-group-zone us-west1-b \
+       --balancing-mode RATE     --max-rate-per-endpoint 5
+
+gcloud compute url-maps create example-grpc-url-map --default-service example-grpc-service
+
+gcloud compute url-maps add-path-matcher example-grpc-url-map --default-service  example-grpc-service \
+       --path-matcher-name example-grpc-path-matcher
+
+gcloud compute url-maps add-host-rule example-grpc-url-map --hosts example-grpc-server:8000 \
+       --path-matcher-name example-grpc-path-matcher
+
+gcloud compute target-grpc-proxies create example-grpc-proxy --url-map example-grpc-url-map
+
+gcloud compute forwarding-rules create example-grpc-forwarding-rule --global \
+  --load-balancing-scheme=INTERNAL_SELF_MANAGED --address=0.0.0.0 \
+  --target-grpc-proxy=example-grpc-proxy --ports 8000 \
+  --network default
+
+# Create MTLS policy on the server side and attach to an ECS
+gcloud alpha network-security server-tls-policies import server_mtls_policy \
+  --source=ug-example/server-mtls-policy.yaml --location=global
+
+gcloud alpha network-services endpoint-config-selectors import ecs_mtls_psms \
+  --source=ug-example/ecs-mtls-psms.yaml --location=global
+
+echo Now enter the zatar-grpc-server pod shell and run the server as follows:
+echo /build/install/example-xds/bin/xds-hello-world-server --xds-creds 8000 my-test-xds-server
+
+#echo Once server is running, enter zatar-grpc-client pod shell and run client:
+#echo /build/install/example-xds/bin/xds-hello-world-client --xds-creds my-xds-client xds:///example-grpc-server:8000
