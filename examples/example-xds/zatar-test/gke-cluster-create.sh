e18c3bc1608eae1ea3c6acba06bda1e621d4c70a
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2021-01-11 10:27:53 -0800

    use the new project bct-gcestaging-guitar-grpc and misc changes for health checks etc

--- examples/example-xds/zatar-test/gke-cluster-create.sh
+++ examples/example-xds/zatar-test/gke-cluster-create.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -x
 
-PROJECT=bct-staging-td-gke-consumer
-CLUSTER_NAME=zatar-staging-nov24
+PROJECT=bct-gcestaging-guitar-grpc
+CLUSTER_NAME=bct-staging-zatar-jan5
 CLUSTER_ZONE=us-west1-pj1
-CA_NAME=pkcs2-ca-staging
+CA_NAME=pkcs-ca-bct-staging-jan5
 CA_ZONE=us-central1
 CA_URL=//privateca.googleapis.com/projects/${PROJECT}/locations/${CA_ZONE}/certificateAuthorities/${CA_NAME}
 
 # To create a cluster with workload identity and managed SPIFFE certs enabled
 /google/data/ro/teams/cloud-sdk/gcloud beta container clusters create ${CLUSTER_NAME} \
-  --cluster-version=1.18.10-gke.1500 --image-type=cos_containerd   --release-channel=rapid \
+  --cluster-version=1.18.12-gke.1200 --image-type=cos_containerd   --release-channel=rapid \
   --workload-pool=${PROJECT}.svc.id.goog   --workload-identity-certificate-authority=${CA_URL} \
-  --zone=${CLUSTER_ZONE} --scopes=cloud-platform   --enable-ip-alias --workload-metadata=GCE_METADATA
+  --zone=${CLUSTER_ZONE} --scopes=cloud-platform   --enable-ip-alias \
+  --tags=allow-health-checks --workload-metadata=GCE_METADATA
 
 /google/data/ro/teams/cloud-sdk/gcloud container clusters get-credentials ${CLUSTER_NAME} --zone=${CLUSTER_ZONE}
 
