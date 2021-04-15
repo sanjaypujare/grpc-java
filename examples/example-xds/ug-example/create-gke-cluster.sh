fc76a655b0fefc5976c9eeb73d8d44cad52571fa
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2021-04-15 13:21:40 -0700

    new create cluster command added

--- examples/example-xds/ug-example/create-gke-cluster.sh
+++ examples/example-xds/ug-example/create-gke-cluster.sh
@@ -1,20 +1,16 @@
 #!/bin/bash -x
 
-ME=sanjaypujare@google.com
-PROJECT=meshca-gke-test
-SA_NAME=me-sa-name    # Needs to be at least 6 characters.
-CLUSTER_NAME=zatar-neg-dec14
-CLUSTER_ZONE=us-west1-a
-CA_NAME=pkcs2-ca
-CA_ZONE=us-west1
-SA=${SA_NAME}@${PROJECT}.iam.gserviceaccount.com
-CA_URL=//privateca.googleapis.com/projects/${PROJECT}/locations/${CA_ZONE}/certificateAuthorities/${CA_NAME}
+PROJECT=`gcloud config get-value project`
+CLUSTER_NAME=zatar-apr15
+CLUSTER_ZONE=us-west1-b
 
 # To create a cluster with workload identity and managed SPIFFE certs enabled
-gcloud beta container clusters create ${CLUSTER_NAME} --cluster-version=1.18.12-gke.1200 \
-  --image-type=cos_containerd --release-channel=rapid --workload-pool=${PROJECT}.svc.id.goog \
-  --workload-identity-certificate-authority=${CA_URL} --impersonate-service-account=${SA} \
-  --zone=${CLUSTER_ZONE} --scopes=cloud-platform  --enable-ip-alias \
+# note we had performed the following override:
+#gcloud config set api_endpoint_overrides/container https://test-container.sandbox.googleapis.com/
+gcloud beta container clusters create ${CLUSTER_NAME} --cluster-version=1.20.5-gke.1300 \
+  --release-channel=rapid --workload-pool=${PROJECT}.svc.id.goog \
+  --enable-workload-certificates --zone=${CLUSTER_ZONE} \
+  --scopes=cloud-platform  --enable-ip-alias \
   --tags=allow-health-checks --workload-metadata=GCE_METADATA
 
 gcloud container clusters get-credentials ${CLUSTER_NAME} --zone=${CLUSTER_ZONE}
